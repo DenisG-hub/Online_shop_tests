c6fece28ddf2628f81185eb7f875e3773d7acf0f
Denis Goncharov <refrom@mail.ru>
2021-07-12 10:55:52 +0300

    final commit

--- conftest.py
+++ conftest.py
@@ -5,13 +5,13 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--lang', action='store', default='ru',
+    parser.addoption('--language', action='store', default='ru',
                      help="Choose lang: es or ru")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    lang = request.config.getoption("lang")
+    lang = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': lang})
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
